07034be555681c5e07ecc1b89ee075a030bdc192
vista497 <vista497@yandex.ru>
2021-10-31 20:45:14 +0300

    make it better

--- config.py
+++ config.py
@@ -3,14 +3,14 @@ from boto.s3.connection import S3Connection
 import os
 
 #DataBase
-# url=os.environ['db_Kate']
-# result=urlparse(url)
-# USER = result.username
-# PASS = result.password
-# DATABASE = result.path[1:]
-# HOST = result.hostname
-# PORT = result.port
+url=S3Connection(os.environ["DATABASE_URL"])
+result=urlparse(url)
+USER = result.username
+PASS = result.password
+DATABASE = result.path[1:]
+HOST = result.hostname
+PORT = result.port
 
 #ApiKey
-SPEECHKIT=S3Connection(os.environ['apiKey_speechKit'])
-TELEGRAM=S3Connection(os.environ['apiKey_tg'])
+SPEECHKIT=S3Connection(os.environ["apiKey_speechKit"])
+TELEGRAM=S3Connection(os.environ["apiKey_tg"])
