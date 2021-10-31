f3c7024b40966d16f4357c0611516e7df387888c
vista497 <vista497@yandex.ru>
2021-10-31 12:08:43 +0300

    update

--- ekaterina.py
+++ ekaterina.py
@@ -1,4 +1,4 @@
-from vosk import Model, KaldiRecognizer  # оффлайн-распознавание от Vosk
+# from vosk import Model, KaldiRecognizer  # оффлайн-распознавание от Vosk
 import speech_recognition as sr # распознавание пользовательской речи (Speech-To-Text)
 import wave  # создание и чтение аудиофайлов формата wav
 import json  # работа с json-файлами и json-строками
@@ -151,44 +151,44 @@ class Kate():
 
             # в случае проблем с доступом в Интернет происходит попытка 
             # использовать offline-распознавание через Vosk
-            except sr.RequestError:
-                print("Trying to use offline recognition...")
-                recognized_data = self.use_offline_recognition()
+            # except sr.RequestError:
+            #     print("Trying to use offline recognition...")
+            #     recognized_data = self.use_offline_recognition()
 
             return recognized_data
 
 
-    def use_offline_recognition(self):
-        """
-        Переключение на оффлайн-распознавание речи
-        :return: распознанная фраза
-        """
-        recognized_data = ""
-        try:
-            # проверка наличия модели на нужном языке в каталоге приложения
-            if not os.path.exists("models/vosk-model-small-ru-0.4"):
-                print("Please download the model from:\n"
-                    "https://alphacephei.com/vosk/models and unpack as 'model' in the current folder.")
-                exit(1)
+    # def use_offline_recognition(self):
+    #     """
+    #     Переключение на оффлайн-распознавание речи
+    #     :return: распознанная фраза
+    #     """
+    #     recognized_data = ""
+    #     try:
+    #         # проверка наличия модели на нужном языке в каталоге приложения
+    #         if not os.path.exists("models/vosk-model-small-ru-0.4"):
+    #             print("Please download the model from:\n"
+    #                 "https://alphacephei.com/vosk/models and unpack as 'model' in the current folder.")
+    #             exit(1)
 
-            # анализ записанного в микрофон аудио (чтобы избежать повторов фразы)
-            wave_audio_file = wave.open("microphone-results.wav", "rb")
-            model = Model("models/vosk-model-small-ru-0.4")
-            offline_recognizer = KaldiRecognizer(model, wave_audio_file.getframerate())
+    #         # анализ записанного в микрофон аудио (чтобы избежать повторов фразы)
+    #         wave_audio_file = wave.open("microphone-results.wav", "rb")
+    #         model = Model("models/vosk-model-small-ru-0.4")
+    #         offline_recognizer = KaldiRecognizer(model, wave_audio_file.getframerate())
 
-            data = wave_audio_file.readframes(wave_audio_file.getnframes())
-            if len(data) > 0:
-                if offline_recognizer.AcceptWaveform(data):
-                    recognized_data = offline_recognizer.Result()
+    #         data = wave_audio_file.readframes(wave_audio_file.getnframes())
+    #         if len(data) > 0:
+    #             if offline_recognizer.AcceptWaveform(data):
+    #                 recognized_data = offline_recognizer.Result()
 
-                    # получение данных распознанного текста из JSON-строки
-                    # (чтобы можно было выдать по ней ответ)
-                    recognized_data = json.loads(recognized_data)
-                    recognized_data = recognized_data["text"]
-        except:
-            print("Sorry, speech service is unavailable. Try again later")
+    #                 # получение данных распознанного текста из JSON-строки
+    #                 # (чтобы можно было выдать по ней ответ)
+    #                 recognized_data = json.loads(recognized_data)
+    #                 recognized_data = recognized_data["text"]
+    #     except:
+    #         print("Sorry, speech service is unavailable. Try again later")
 
-        return recognized_data
+    #     return recognized_data
 
 
 if __name__ == "__main__":
