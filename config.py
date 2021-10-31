fac28166aa00baa1b1e89b1715021f77e52d9302
vista497 <vista497@yandex.ru>
2021-10-31 19:55:39 +0300

    make

--- config.py
+++ config.py
@@ -1,8 +1,9 @@
 from urllib.parse import urlparse
+from boto.s3.connection import S3Connection
 import os
 
 #DataBase
-result=urlparse(os.environ.set('DATABASE_URL'))
+result=urlparse(os.environ['DATABASE_URL'])
 USER = result.username
 PASS = result.password
 DATABASE = result.path[1:]
@@ -10,5 +11,5 @@ HOST = result.hostname
 PORT = result.port
 
 #ApiKey
-SPEECHKIT=os.environ.set('apiKey_speechKit')
-TELEGRAM=os.environ.set('apiKey_tg')
+SPEECHKIT=os.environ['DATABASE_URL']
+TELEGRAM=os.environ['DATABASE_URL']
